cbbf2058d1b5961284b670a8c22fbb9e66a5146e
alper1616 <alper_pacaci@hotmail.com>
2023-01-14 13:57:38 -0500

    New private info and test case added!

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -1,4 +1,8 @@
 package utils;
 
 public class MainUtils {
+
+    private String name ;
+    private int salary ;
+
 }
---
+++ src/utils/Test.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Test {
+
+    // Test case very sexy!
+}
